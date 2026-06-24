4d332397c2794ae2758ce69c9c06737ad3f77ac2
Amiya Ranjan Sing Nayak <nayakarup172@gmail.com>
2026-06-24 21:58:32 +0530

    Fix News Section

--- services/news.ts
+++ services/news.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_GNEWS_API_KEY;
 
 export const fetchNews = async () => {
   const response = await axios.get(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=${API_KEY}`
   );
 
   return response.data.articles;
